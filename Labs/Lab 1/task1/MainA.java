7cb8eaf5cfd8df141e7d6b5c8e02353a95f69515
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-08-31 14:51:26 +0200

    Task 1.1

--- Labs/Lab 1/task1/MainA.java
+++ Labs/Lab 1/task1/MainA.java
@@ -1,10 +1,35 @@
 public class MainA {
-	public static class Incrementer implements Runnable {
-		public void run() {
-		}
-	}
+	// Create a volatile integer counter that will be shared between threads using the keyword static volatile
+    private static volatile int counter = 0;
 
-	public static void main(String [] args) {
+    public static class Incrementer implements Runnable {
+        public void run() {
+			// Increment the counter 1,000,000 times
+            for (int i = 0; i < 1000000; i++) {
+                counter++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+		// Create 4 threads and store them in an array
+        Thread[] threads = new Thread[4];
+
+		// Loop over the threads and start them with the Incrementer runnable
+        for (int i = 0; i < 4; i++) {
+            threads[i] = new Thread(new Incrementer());
+            threads[i].start();
+        }
 		
-	}
+		// Loop over the threads and join them
+        for (int i = 0; i < 4; i++) {
+            try {
+				threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Final value of counter: " + counter);
+    }
 }
