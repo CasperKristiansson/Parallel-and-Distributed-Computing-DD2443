36d5cd47512cd0abbe4319c21c19ba3d3781a890
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-09-01 12:28:03 +0200

    Task 4
    Co-authored-by: Ninni <NicoleWij@users.noreply.github.com>

--- Labs/Lab 1/task4/CountingSemaphore.java
+++ Labs/Lab 1/task4/CountingSemaphore.java
@@ -1,14 +1,40 @@
 public class CountingSemaphore {
+    private int count; // Semaphore counter
 
-	public CountingSemaphore(int n) {
-		// TODO
+    public CountingSemaphore(int n) {
+        this.count = n;
+    }
+
+	public synchronized int getCount() {
+		return count;
 	}
 
-	public void signal() {
-		// TODO
-	}
+    public synchronized void signal() {
+		// Increment the semaphore counter
+        count++;
+		System.out.println(Thread.currentThread().getId() + ": signal count: " + count);
+		// If the counter is greater than 0, notify one thread
+        if (count <= 0) {
+			System.out.println(Thread.currentThread().getId() + ": notifying");
+            notify();
+        }
+    }
 
-	public void s_wait() {
-		// TODO
-	}
+    public synchronized void s_wait() {
+		// Decrement the semaphore counter
+        count--;
+		System.out.println(Thread.currentThread().getId() + ": wait count: " + count);
+		// If the counter is less than 0, wait
+        while (count < 0) {
+            try {
+				// Wait until the semaphore counter is greater than 0
+				System.out.println(Thread.currentThread().getId() + ": waiting");
+                wait();
+				System.out.println(Thread.currentThread().getId() + ": woke up");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
+    }
 }
--- Labs/Lab 1/task4/Main.java
+++ Labs/Lab 1/task4/Main.java
@@ -1,10 +1,31 @@
 public class Main {
 
-	public static class Runner implements Runnable {
-		public void run() {
-		}
-	}
+    public static class Runner implements Runnable {
+        private final CountingSemaphore semaphore;
 
-	public static void main(String [] args) {
-	}
+        public Runner(CountingSemaphore semaphore) {
+            this.semaphore = semaphore;
+        }
+
+        public void run() {
+            semaphore.s_wait();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            semaphore.signal();
+            System.out.println(Thread.currentThread().getId() + ": releasing the semaphore, count: " + semaphore.getCount());
+        }
+    }
+
+    public static void main(String[] args) {
+        CountingSemaphore semaphore = new CountingSemaphore(3);
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runner(semaphore)).start();
+        }
+    }
 }
