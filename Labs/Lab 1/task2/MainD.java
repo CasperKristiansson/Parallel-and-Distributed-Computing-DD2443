7135cb7f663fa3b12a9ab5f1b3c460f44420263a
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-09-03 16:23:09 +0200

    Task 2 add comments

--- Labs/Lab 1/task2/MainD.java
+++ Labs/Lab 1/task2/MainD.java
@@ -1,16 +1,19 @@
 public class MainD {
+    // Create a static integer counter that will be shared between threads
     private static int sharedInt = 0;
     private static volatile boolean done = false; 
     private static boolean doneGuarded = false;
 
     public static class Incrementer implements Runnable {
         public void run() {
+            // Increment the sharedInt 1000000 times
             for (int i = 0; i < 1000000; i++) {
                 sharedInt++;
             }
 
             done = true;
 
+            // Notify the printingThread of completion
             synchronized (MainD.class) {
                 doneGuarded = true;
                 MainD.class.notify();
@@ -43,6 +46,7 @@ public class MainD {
         sharedInt = 0;
         done = false;
 
+        // Create threads for incrementing and printing
         Thread incrementingThread = new Thread(new Incrementer());
         Thread printingThread = new Thread(new PrinterBusyWaiting());
 
@@ -61,6 +65,7 @@ public class MainD {
         sharedInt = 0;
         doneGuarded = false;
 
+        // Create threads for incrementing and printing
         Thread incrementingThread = new Thread(new Incrementer());
         Thread printingThread = new Thread(new PrinterGuardedBlock());
 
@@ -78,6 +83,7 @@ public class MainD {
     public static void main(String[] args) {
         MainD mainD = new MainD();
 
+        // Warmup X times, Experiment Y times
         int X = 10;
         int Y = 1000;
 
@@ -87,13 +93,14 @@ public class MainD {
 
         System.out.println("Busy-Waiting");
         long totalBusyWaitingTime = 0;
+
         for (int i = 0; i < Y; i++) {
             long startTime = System.nanoTime();
             mainD.runExperimentsBusyWaiting();
             long endTime = System.nanoTime();
             long delay = endTime - startTime;
             totalBusyWaitingTime += delay;
-            System.out.println("Total Delay (Busy-Waiting) for run " + (i + 1) + " in nanoseconds: " + delay);
+            System.out.println("Tot: (Busy-Waiting) for run " + (i + 1) + " in nanoseconds: " + delay);
         }
 
         for (int i = 0; i < X; i++) {
@@ -102,20 +109,20 @@ public class MainD {
 
         System.out.println("\nGuarded Block");
         long totalGuardedBlockTime = 0;
+
         for (int i = 0; i < Y; i++) {
             long startTime = System.nanoTime();
             mainD.runExperimentsGuardedBlock();
             long endTime = System.nanoTime();
             long delay = endTime - startTime;
             totalGuardedBlockTime += delay;
-            System.out.println("Total Delay (Guarded Block) for run " + (i + 1) + " in nanoseconds: " + delay);
+            System.out.println("Tot: (Guarded Block) for run " + (i + 1) + " in nanoseconds: " + delay);
         }
 
         double averageBusyWaitingTime = totalBusyWaitingTime / (double) Y;
         double averageGuardedBlockTime = totalGuardedBlockTime / (double) Y;
 
-        System.out.println("\n==== Averages ====");
-        System.out.println("Average delay (Busy-Waiting)  in nanoseconds: " + averageBusyWaitingTime);
-        System.out.println("Average delay (Guarded Block) in nanoseconds: " + averageGuardedBlockTime);
+        System.out.println("Avg: (Busy-Waiting)  in nanoseconds: " + averageBusyWaitingTime);
+        System.out.println("Avg: (Guarded Block) in nanoseconds: " + averageGuardedBlockTime);
     }
 }
