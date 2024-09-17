0b21002b67acb3185b4e85f1c5ed4fc3ce499464
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-09-17 21:52:37 +0200

    Task 4 Sorting Done

--- Labs/Lab 2/src/ForkJoinPoolSort.java
+++ Labs/Lab 2/src/ForkJoinPoolSort.java
@@ -1,31 +1,74 @@
-
-/**
- * Sort using Java's ForkJoinPool.
- */
-
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 public class ForkJoinPoolSort implements Sorter {
-	public final int threads;
+    public final int threads;
+    private ForkJoinPool forkJoinPool;
 
-	public ForkJoinPoolSort(int threads) {
-		this.threads = threads;
-	}
+    public ForkJoinPoolSort(int threads) {
+        this.threads = threads;
+    }
 
-	public void sort(int[] arr) {
-		// TODO: sort arr.
-	}
+    @Override
+    public void sort(int[] arr) {
+        this.forkJoinPool = new ForkJoinPool(this.threads);
 
-	public int getThreads() {
-		return threads;
-	}
+        forkJoinPool.invoke(new Worker(arr, 0, arr.length - 1));
+    }
 
-	private static class Worker extends RecursiveAction {
-		Worker() {
-		}
+    @Override
+    public int getThreads() {
+        return threads;
+    }
 
-		protected void compute() {
-		}
-	}
+    private static class Worker extends RecursiveAction {
+        private final int[] arr;
+        private final int low, high;
+
+        Worker(int[] arr, int low, int high) {
+            this.arr = arr;
+            this.low = low;
+            this.high = high;
+        }
+
+        @Override
+        protected void compute() {
+            if (high - low <= 1000) {
+                sequentialQuickSort(arr, low, high);
+            } else {
+                int partitionIndex = partition(arr, low, high);
+                Worker leftTask = new Worker(arr, low, partitionIndex - 1);
+                Worker rightTask = new Worker(arr, partitionIndex + 1, high);
+                invokeAll(leftTask, rightTask);
+            }
+        }
+
+        private void sequentialQuickSort(int[] arr, int low, int high) {
+            if (low < high) {
+                int partitionIndex = partition(arr, low, high);
+                sequentialQuickSort(arr, low, partitionIndex - 1);
+                sequentialQuickSort(arr, partitionIndex + 1, high);
+            }
+        }
+
+        private int partition(int[] arr, int low, int high) {
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++) {
+                if (arr[j] < pivot) {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+
+            return i + 1;
+        }
+    }
 }
