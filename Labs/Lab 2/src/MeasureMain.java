2793d23405d7ff2b8d2703789fe6a81ff40bf3be
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-09-17 22:28:12 +0200

    ParallelStream sort Task 5

--- Labs/Lab 2/src/MeasureMain.java
+++ Labs/Lab 2/src/MeasureMain.java
@@ -3,7 +3,7 @@ public class MeasureMain {
 	public static void main(String[] args) {
 		// overwrite the args if they are not provided
 		if (args.length == 0) {
-			args = new String[] { "ForkJoinPool", "64", "10000", "5", "100", "10" };
+			args = new String[] { "ParallelStream", "64", "10000", "5", "100", "10" };
 		}
 		// Sorter name
 		String name = args[0];
--- Labs/Lab 2/src/ParallelStreamSort.java
+++ Labs/Lab 2/src/ParallelStreamSort.java
@@ -1,3 +1,4 @@
+
 /**
  * Sort using Java's ParallelStreams and Lambda functions.
  *
@@ -13,19 +14,71 @@
  * ForkJoinPool myPool = new ForkJoinPool(threads);
  * myPool.submit(() -> "my parallel stream method / function");
  */
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
 
 public class ParallelStreamSort implements Sorter {
-	public final int threads;
+    private final int threads;
 
-	public ParallelStreamSort(int threads) {
-		this.threads = threads;
-	}
+    public ParallelStreamSort(int threads) {
+        this.threads = threads;
+    }
 
-	public void sort(int[] arr) {
-		// TODO: sort arr.
-	}
+    @Override
+    public void sort(int[] arr) {
+        ForkJoinPool pool = new ForkJoinPool(threads);
+        pool.submit(() -> parallelQuickSort(arr, 0, arr.length - 1)).join();
+		pool.shutdown(); 
+    }
 
-	public int getThreads() {
-		return threads;
-	}
+    private void parallelQuickSort(int[] arr, int low, int high) {
+        if (low < high) {
+			if (high - low <= 1000) {
+				sequentialQuickSort(arr, low, high);
+			} else {
+				int pivotIndex = partition(arr, low, high);
+
+				// Arrays.asList(
+				// 	new int[] {low, pivotIndex - 1},
+				// 	new int[] {pivotIndex + 1, high}
+				// ).parallelStream().forEach(bounds -> parallelQuickSort(arr, bounds[0], bounds[1]));
+				Arrays.stream(new int[][]{{low, pivotIndex - 1}, {pivotIndex + 1, high}})
+                    .parallel()
+                    .forEach(parts -> parallelQuickSort(arr, parts[0], parts[1]));
+			}
+        }
+    }
+
+	private void sequentialQuickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(arr, low, high);
+            sequentialQuickSort(arr, low, partitionIndex - 1);
+            sequentialQuickSort(arr, partitionIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    @Override
+    public int getThreads() {
+        return threads;
+    }
 }
